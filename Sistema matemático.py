827f115c29884307e47e0f1bc373c47af06d8aa0
alebs10 <alexandre2002.ab@gmail.com>
2022-09-12 18:43:34 -0300

    Sistema Matematico.

---
+++ Sistema matemático.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("*** BEM-VINDO AO SISTEMA***")
+
+a = float(input("DIGITE O VALOR DE A: "))
+b = float(input("DIGITE O VALOR DE B: "))
+c = float(input("DIGITE O VALOR DE C: "))
+
+r = (a + b)**2
+s = (b + c)**2
+
+print("O VALOR DE R: ", r)
+print("O VALOR DE S: ", s)
+
+print("ENTÃO...")
+
+d = (r + s)/2
+
+print("O VALOR DE D: ", d)
+
+
+# In[ ]:
+
+
+
+
